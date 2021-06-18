169b29460e25c87b2c0f00ff587aeed0f3dd2f6f
timoMil <timo_milbich@gmx.de>
2021-06-18 16:23:31 +0200

    added deit architecture

--- runs/debug.sh
+++ runs/debug.sh
@@ -1,7 +1,7 @@
-export GPU_TRAINING=2,
+export GPU_TRAINING=5,
 echo "GPUs: ${GPU_TRAINING}"
 export EXP_PATH='/export/data/tmilbich/PycharmProjects/dml_pl/experiments/training_models'
 echo "EXP_PATH: ${EXP_PATH}"
 
 ### DEBUGGING
-python main.py --savename baseline_marginloss_cub200 --exp_path ${EXP_PATH} --gpus ${GPU_TRAINING} --base configs/marginloss.yaml
+python main.py --savename debub_deitS_marginloss_cub200 --exp_path ${EXP_PATH} --gpus ${GPU_TRAINING} --base configs/marginloss.yaml
